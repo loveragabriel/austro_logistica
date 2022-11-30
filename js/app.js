41cad835d6c2733015ba9bafd8e4e8071249e9f5
Gabriel <loveragabriel20@gmail.com>
2022-11-30 07:02:38 -0300

    working on sidebar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-const menu = document.getElementById("sidebar");
+const menu = document.getElementById("menu-btn");
+const sideBar = document.getElementById("closeMenu");
 
 const displayMenu =()=>{
-  document.getElementById("sidebar").style.display = "flex"; 
+ document.getElementById("sidebar").style.display = "flex"; 
+}
+
+const closeMenu = ()=>{
+  document.getElementById("sidebar").style.display = "none";
 }
 menu.onclick = displayMenu; 
+sideBar.onclick = closeMenu;
